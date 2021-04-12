b20f6808c6fffa4bd63e44afc5bbc221d21e8c16
Customer <503720+graiz@users.noreply.github.com>
2021-04-12 05:30:13 +0000

    removed WSJ, no longer supported. :(  added WA-PO. :)

--- index.php
+++ index.php
@@ -10,12 +10,12 @@ date_default_timezone_set('Etc/GMT+4');  // adjust for server timezone
 // https://cdn.freedomforum.org/dfp/pdf16/MA_BG.pdf
 
 $news = array();
-$paper['prefix']="WSJ";$paper['style']="width:98%;margin:-70px 0px 0px -15px";array_push($news,$paper);		// Wall St Journal
-$paper['prefix']="MA_BG";$paper['style']="width:98%;margin:5px 10px 0px 8px";array_push($news,$paper);		// Boston Globe
-$paper['prefix']="NY_NYT";$paper['style']="width:99%;margin:-88px 14px 0px 3px";array_push($news,$paper);	// New York Times
-$paper['prefix']="CA_LAT";$paper['style']="width:94%;margin:-2% 0px 0px 0px";array_push($news,$paper);		// L.A. Times
-$paper['prefix']="CAN_TS";$paper['style']="width:90%;margin:-70px 0px 0px 0px";array_push($news,$paper);	// Toronto Star
-$paper['prefix']="CA_SFC";$paper['style']="width:96%;margin:-20px 0px 0px 0px";array_push($news,$paper);  	// San Fran Chronical
+$paper['prefix']="DC_WP";$paper['style']="width:108%;margin:-5% -5% 0px -5%";array_push($news,$paper);     // Washington Post 
+$paper['prefix']="MA_BG";$paper['style']="width:98%;margin:5px 10px 0px 8px";array_push($news,$paper);	   // Boston Globe
+$paper['prefix']="NY_NYT";$paper['style']="width:99%;margin:-88px 14px 0px 3px";array_push($news,$paper);  // New York Times
+$paper['prefix']="CA_LAT";$paper['style']="width:94%;margin:-2% 0px 0px 0px";array_push($news,$paper);	   // L.A. Times
+$paper['prefix']="CAN_TS";$paper['style']="width:90%;margin:-70px 0px 0px 0px";array_push($news,$paper);   // Toronto Star
+$paper['prefix']="CA_SFC";$paper['style']="width:96%;margin:-20px 0px 0px 0px";array_push($news,$paper);   // San Fran Chronical
 
 $maxPapers = count($news) -1;
 
